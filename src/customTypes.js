77672a6a2b1955bfe665fcc860b181438bab34bf
anbreaker <antunez19@gmail.com>
2021-10-11 17:25:32 +0200

    custom types jsdoc

---
+++ src/customTypes.js
@@ -0,0 +1,28 @@
+/**
+ * A New User Object
+ * @typedef {Object} User
+ * @property {number} id User id
+ * @property {string} name User's name
+ * @property {number | string} [age] User's age (Optional)
+ * @property {boolean} isActive User state
+ */
+
+/**
+ * @type {User}
+ */
+const myNewUser = {
+  id: 1,
+  name: "anbreaker",
+  age: 35,
+  isActive: true,
+}
+
+/**
+ * @type {User}
+ */
+const myNewUser2 = {
+  id: 2,
+  name: "Magalí",
+  age: 1,
+  isActive: true,
+}
--- src/functions.js
+++ src/functions.js
@@ -2,8 +2,16 @@
  * Add two number
  * @param {Number} n1 first param
  * @param {Number} n2 second param
- * @returns {Number}
+ * @returns {Number} Total result
  */
 const add = (n1, n2) => {
   return n1 + n2
 }
+
+/**
+ * Add two number and returns a string
+ * @param {Number} n1 first number
+ * @param {Number} n2 second number
+ * @returns {String} Total result
+ */
+const addTwo = (n1, n2) => `Result is: ${n1 + n2}`
