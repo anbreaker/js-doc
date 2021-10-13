701db9332c6e57bd468a7788471a0a7ca3fa88e6
anbreaker <antunez19@gmail.com>
2021-10-13 09:50:08 +0200

    jsdoc templates

---
+++ src/libs/myMath.js
@@ -0,0 +1,36 @@
+/**
+ * My Math library
+ * @module
+ */
+
+/**
+ * Add two number
+ * @param {number} x First number
+ * @param {number} y second number
+ * @returns {number} Sum of X and Y
+ */
+const add = (x, y) => x + y
+
+/**
+ * Subtract two number
+ * @param {number} x First number
+ * @param {number} y second number
+ * @returns {number} Subtract of X and Y
+ */
+const subtract = (x, y) => x - y
+
+/**
+ * Product two number
+ * @param {number} x First number
+ * @param {number} y second number
+ * @returns {number} Product of X and Y
+ */
+const product = (x, y) => x * y
+
+/**
+ * Division two number
+ * @param {number} x First number
+ * @param {number} y second number
+ * @returns {number} Division of X and Y
+ */
+const division = (x, y) => x / y
